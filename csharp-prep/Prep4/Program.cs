a2e96ea235b5a9ebb9dd602df28abf87f4801b98
kelechiimo2006 <43006809+kelechiimo2006@users.noreply.github.com>
2025-11-03 20:35:56 +0000

    finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+
+        do
+        {
+            Console.Write("What is your number? ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        } while (userNumber != 0);
+        //foreach (int number in numbers)
+        {
+            //Console.Write($"{number},");
+        }
+        Console.WriteLine($"The sum is {numbers.Sum()}");
+        Console.WriteLine($"The average is {numbers.Average()}");
+        Console.WriteLine($"The largest number is {numbers.Max()}");
     }
 }
