d5f9583c2389b4df782c221fdd4fd239640f2594
kelechiimo2006 <43006809+kelechiimo2006@users.noreply.github.com>
2025-11-11 23:46:36 +0000

    finished learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Resume myresume = new Resume();
+        Job job1 = new Job();
+        Job job2 = new Job();
+        myresume._name = "Samuel";
+        job1._company = "Amazon";
+        job1._endYear = 2006;
+        job1._startYear = 2001;
+        job1._jobTitle = "Officer";
+
+        job2._company = "Finance";
+        job2._endYear = 2003;
+        job2._startYear = 2005;
+        job2._jobTitle = "icer";
+        myresume._jobs.Add(job1);
+        myresume._jobs.Add(job2);
+
+        myresume.Display();
+
     }
 }
