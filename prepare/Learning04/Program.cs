d6121e217a3be94e63cbac7443dababb5e5e4b61
kelechiimo2006 <43006809+kelechiimo2006@users.noreply.github.com>
2025-11-28 23:28:17 +0000

    findished prep for inheritnace

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Peter", "Love");
+        MathAssignment mathAssignment = new MathAssignment("Samuel", "Fractions", "4.2", "7-9");
+        WritingAssignment writingAssignment = new WritingAssignment("Paul", "Letters", "How to Write Comfortably");
+
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine("");
+        
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine("");
+
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine("");
     }
 }
