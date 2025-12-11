bef1aa7958d3b066974c57d7924b692dcb880434
Samuel-David Imo <kelechiimo2006@gmail.com>
2025-12-11 14:45:49 -0700

    finished learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List <Shape> _shapes = new List<Shape>();
+        Square square1 = new Square("blue", 2);
+        Circle circle1 = new Circle("red", 3);
+        Rectangle rectangle1 = new Rectangle("white", 4,7);
+
+        _shapes.Add(square1);
+        _shapes.Add(circle1);
+        _shapes.Add(rectangle1);
+
+        foreach (Shape shape in _shapes)
+        {
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine("");
+        }
     }
 }
---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,5 @@
+abstract class Goal
+{
+    private string _name;
+    
+}
