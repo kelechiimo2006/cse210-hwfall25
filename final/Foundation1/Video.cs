a3ab379e8054c9d1c36f0eb93cccb4cf69371927
kelechiimo2006 <43006809+kelechiimo2006@users.noreply.github.com>
2025-12-12 01:32:12 +0000

    fixed some List errors

--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -4,7 +4,7 @@ public class Video
 public string _title;
 public string _author;
 public int _length;
-public List<Comment> _comments = new Comment();
+public List<Comment> _comments = new List<Comment>();
 
 public int AmountOfComments()
 {
