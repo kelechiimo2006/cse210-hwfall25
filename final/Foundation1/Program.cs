b95375021f67bde78f087f819831fbc0f4cc1463
kelechiimo2006 <43006809+kelechiimo2006@users.noreply.github.com>
2025-12-12 02:01:30 +0000

    finished foundation 1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<Video> _videos = new List<Video>();
+
+        Video video1 = new Video()
+        video1._title = "Learn Bolivian Cooking";
+        video1._author = "Jorge Gonzales";
+        video1._length = 120;
+        Comment comment1V1 = new Comment();
+        comment1V1._name = "Sam";
+        comment1V1._text = "Bolivain cooking seems so fun";
+        video1._comments.Add(comment1V1);
         
+        Comment comment2V1 = new Comment();
+        comment2V1._name = "Paul";
+        comment2V1._text = "How can i order the ingredients";
+        video1._comments.Add(comment2V1);
+        _videos.Add(video1);
+
+
+        foreach (Video video in _author)
+        {
+            Console.WriteLine($"{video._title} {video._author} {video._length}");
+            Console.WriteLine(video.AmountOfComments());
+        }
     }
 }
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -1,14 +1,14 @@
 public class Video 
 {
 
-public string _title;
-public string _author;
-public int _length;
-public List<Comment> _comments = new List<Comment>();
+    public string _title;
+    public string _author;
+    public int _length;
+    public List<Comment> _comments = new List<Comment>();
 
 public int AmountOfComments()
 {
-    return _comments.Count()
+    return _comments.Count();
 }
 
 }
