3debe886b5caaf2e6fedf0a7c21127b9a330edb0
kelechiimo2006 <43006809+kelechiimo2006@users.noreply.github.com>
2025-12-12 02:50:48 +0000

    finished foundation 1

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -1,6 +1,6 @@
 public class Comment
 {
-public string _name;
-public string _text;
+    public string _name;
+    public string _text;
 
 }
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -1,14 +1,13 @@
 public class Video 
 {
-
     public string _title;
     public string _author;
     public int _length;
     public List<Comment> _comments = new List<Comment>();
 
-public int AmountOfComments()
-{
-    return _comments.Count();
-}
+    public int AmountOfComments()
+    {
+        return _comments.Count();
+    }
 
 }
